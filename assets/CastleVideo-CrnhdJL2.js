53cf769e84896e3b681f9ff09be9ea7f0a42e3e1
yaya75315 <dragon.tech.insect@gmail.com>
2025-01-16 00:22:18 +0800

    version 0.2.8

---
+++ assets/CastleVideo-CrnhdJL2.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-gkJngwgL.js";const t={},s={class:"video-container"};function a(n,_){return o(),c("div",s)}const d=e(t,[["render",a],["__scopeId","data-v-f2d1e40c"]]);export{d as default};
