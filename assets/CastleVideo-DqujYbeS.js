6851d832df8861b7b57404dc00d4160b596a704a
yaya75315 <dragon.tech.insect@gmail.com>
2025-01-26 22:44:52 +0800

    version 0.3.6

--- assets/CastleVideo-DqujYbeS.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-D7pmJSUH.js";const t={},s={class:"video-container"};function a(n,_){return o(),c("div",s)}const d=e(t,[["render",a],["__scopeId","data-v-f2d1e40c"]]);export{d as default};
---
+++ assets/CastleVideo-sldXe5Yi.js
@@ -0,0 +1,1 @@
+import{_ as e,c,a as o}from"./index-CH_cLMAe.js";const t={},s={class:"video-container"};function a(n,_){return o(),c("div",s)}const d=e(t,[["render",a],["__scopeId","data-v-f2d1e40c"]]);export{d as default};
